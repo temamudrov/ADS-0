0ce102342a19d3792bc2f8a62ff52d8fc454e06d
temamudrov <79166955+temamudrov@users.noreply.github.com>
2021-02-25 14:51:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int nod = 0;
+	for (int i = 1; i <= a; i++)
+		if (a % i == 0 && b % i == 0)
+			nod = i;
+	return nod;
 }
