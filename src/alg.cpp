51ddc86d679b45972bff08ade228f65e29dbb667
temamudrov <79166955+temamudrov@users.noreply.github.com>
2021-03-18 21:40:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
+// Copyright 2021 NNTU-CS
+
 int gcd(int a, int b) {
-    while (a != b)
-if (a > b)
-a -= b;
-else
-b -= a;
-return a;
+    if (b == 0)
+            return a;
+    else
+            return gcd (b, a % b);
 }
